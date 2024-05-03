4aa5ca9a4e26d0ced0ba48c9e0e149a7705c3624
AirtonDoug <airtonservodecristo@gmail.com>
2024-05-03 11:54:48 -0300

    feat: add prisma.ts file

---
+++ src/app/_components/category-list.tsx
@@ -0,0 +1,12 @@
+const CategoryList = () => {
+    
+    
+    // pega as categorias do banco de daos
+    // pra cada cateogira renderiza um item
+
+    return (  
+
+    );
+}
+ 
+export default CategoryList;
