fecd40d2b5be3e0e4fcab5a6205fd40b4f441b85
AirtonDoug <airtonservodecristo@gmail.com>
2024-05-03 12:26:06 -0300

    feat: add category item and list components

--- src/app/_components/category-list.tsx
+++ src/app/_components/category-list.tsx
@@ -1,11 +1,22 @@
-const CategoryList = () => {
-    
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+
+// apenas server component pode ser async
+const CategoryList = async () => {
+    // acessa todas as categorias do banco de dados
+    const categories = await db.category.findMany({})
     
     // pega as categorias do banco de daos
     // pra cada cateogira renderiza um item
 
     return (  
-
+        <div className="flex overflow-scroll gap-3">
+        {categories.map((category) =>(
+            <CategoryItem key={category.id} category={category} />
+        
+        ))}
+        </div>
     );
 }
  
